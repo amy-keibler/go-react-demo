a890bc6ccf8ccfd075fa4f096d77eed846a2666f
Matthew Keibler <wanderingelf14@gmail.com>
2018-06-22 20:07:25 -0400

    Changes nginx port and adds Luke's siege weapon endpoint

--- README.md
+++ README.md
@@ -4,4 +4,5 @@
 2. Ensure that you have the latest node/npm installed locally
 3. Install VSCode (or a similar editor)
 4. Start up the docker-compose setup via `docker-compose up --build` from the project root
-5. Visit http://localhost/ and http://localhost/api/v1/info to ensure that the server is running correctly
+5. Visit http://localhost:7878/ and http://localhost:7878/api/v1/info to ensure that the server is running correctly
+6. Try submitting siege weapons to http://localhost:7878/rate/{siege_weapon}
--- go-rust/src/main.rs
+++ go-rust/src/main.rs
@@ -14,15 +14,65 @@ fn index(_req: HttpRequest) -> Result<fs::NamedFile> {
 
 #[derive(Serialize)]
 struct ApiInfo {
-    served_by: String,
+    served_by: &'static str,
     number: u8,
 }
 
 fn api(_req: HttpRequest) -> Result<Json<ApiInfo>> {
     Ok(Json(ApiInfo {
-        served_by: String::from("Rust"),
+        served_by: "Rust",
         number: random(),
     }))
+}
+
+#[derive(Serialize)]
+struct SiegeRating {
+    rating: u8,
+    review: &'static str,
+    valid: bool,
+}
+
+fn rate(req: HttpRequest) -> Result<Json<SiegeRating>> {
+    let rating: SiegeRating = req.match_info()
+        .get("siege_weapon")
+        .map(|sw| match sw {
+            "trebuchet" => SiegeRating {
+                rating: 10,
+                review: "The superior siege weapon",
+                valid: true,
+            },
+            "catapult" => SiegeRating {
+                rating: 5,
+                review: "A strictly worse trebuchet",
+                valid: true,
+            },
+            "battering ram" => SiegeRating {
+                rating: 1,
+                review: "Your grand plan is to use a glorified tree as a siege weapon?",
+                valid: true,
+            },
+            "ballista" => SiegeRating {
+                rating: 2,
+                review: "This is not a crossbow rating API. Cool crossbow though.",
+                valid: true,
+            },
+            "helepolis" => SiegeRating {
+                rating: 3,
+                review: "Are you sure this isn't a droid transport ship? Either way, seems bad.",
+                valid: true,
+            },
+            _ => SiegeRating {
+                rating: 0,
+                review: "Not a valid medieval siege weapon",
+                valid: false,
+            },
+        })
+        .unwrap_or(SiegeRating {
+            rating: 0,
+            review: "No weapon provided",
+            valid: false,
+        });
+    Ok(Json(rating))
 }
 
 fn main() {
@@ -30,6 +80,7 @@ fn main() {
     server::new(|| {
         App::new()
             .resource("/", |r| r.method(Method::GET).f(index))
+            .resource("/rate/{siege_weapon}", |r| r.method(Method::GET).f(rate))
             .resource("/api/v1/info", |r| r.method(Method::GET).f(api))
             .handler("/scripts", fs::StaticFiles::new("./static"))
     }).bind("0.0.0.0:8080")
