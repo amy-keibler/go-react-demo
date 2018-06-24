2ff5c07283986496a078e094842ed51ecf797864
Matthew Keibler <wanderingelf14@gmail.com>
2018-06-24 18:05:55 -0400

    Stage 1

--- go-react/src/index.js
+++ go-react/src/index.js
@@ -1,1 +1,3 @@
-console.log("Hello world");
+import { log } from "./logger";
+
+log("Hello GO");
---
+++ go-react/src/logger.ts
@@ -0,0 +1,3 @@
+export function log(message: String): void {
+    console.log("Logging: " + message);
+}
